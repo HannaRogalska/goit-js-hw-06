5f695aaa8b20f1d9affc20ee887daa160563e822
Hanna Rogalska <hanrogalska@gmail.com>
2023-02-20 10:36:34 +0100

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const getBody = document.querySelector("body");
+console.log(getBody);
+const getSpan = document.querySelector(".color");
+console.log(getSpan);
+const getButton = document.querySelector(".change-color");
+console.log(getButton);
+
+getButton.addEventListener("click", () => {
+  getBody.style.backgroundColor = getRandomHexColor();
+  getSpan.textContent = getBody.style.backgroundColor;
+});
